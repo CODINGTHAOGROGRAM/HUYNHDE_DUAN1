ec9bfa5c059ebc773bb938fcb7cc7cb38c3a2efd
MSI <MSI@DESKTOP-VQE9HMG>
2022-11-21 12:21:59 +0700

    3layer_DTO

---
+++ DTO/DTO_NhanVien.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DTO_NhanVien
+    {
+        private string maNV;
+
+        public string MaNV { get => maNV; set => maNV = value; }
+
+        private string ten;
+
+        public string Ten { get => ten; set => ten = value; }
+
+        private string email;
+
+        public string Email { get => email; set => email = value; }
+
+        private string matKhau;
+        public string MatKhau { get => matKhau; set => matKhau = value; }
+
+        private int gioiTinh;
+        public int GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+
+        private string soDienThoai;
+        public string SoDienThoai { get => soDienThoai; set => soDienThoai = value; }
+
+        private DateTime ngaySinh;
+
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+
+        private string diaChi;
+        public string DiaChi { get => diaChi; set => diaChi = value; }
+
+        private int vaiTro;
+        public int VaiTro { get => vaiTro; set => vaiTro = value; }
+
+        private string anh;
+        public string Anh { get => anh; set => anh = value; }
+
+        public DTO_NhanVien(string manv, string ten, string email, string matkhau, int gioitinh, string sodienthoai, DateTime ngaysinh, string diachi, int vaitro, string anh)
+        {
+            this.maNV = manv;
+            this.ten = ten;
+            this.email = email;
+            this.matKhau = matkhau;
+            this.gioiTinh = gioitinh;
+            this.soDienThoai = sodienthoai;
+            this.ngaySinh = ngaysinh;
+            this.diaChi = diachi;
+            this.vaiTro = vaitro;
+            this.anh = anh;
+        }
+        public DTO_NhanVien() { }
+    }
+}

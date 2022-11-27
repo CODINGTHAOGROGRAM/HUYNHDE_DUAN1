b300ee9984272869f5e60c15e4bb172500060ddc
LAMSAMCHAN <chan94170@gmail.com>
2022-11-27 18:35:12 +0700

    1

--- DAL/DAL_GiaoDichTraiPhieu.cs
+++ DAL/DAL_GiaoDichTraiPhieu.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.OleDb;
 using System.Data;
+using System.Data.OleDb;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL
 {
@@ -18,6 +14,25 @@ namespace DAL
             get { if (instance == null) instance = new DAL_GiaoDichTraiPhieu(); return DAL_GiaoDichTraiPhieu.instance; }
 
             private set { DAL_GiaoDichTraiPhieu.instance = value; }
+        }
+
+        public DataTable loadData()
+        {
+            DataTable dtb = new DataTable();
+            string query = "ShowData_GDTP";
+
+            return dtb = DataProvider.Instance.Executequery(query);
+        }
+
+        public bool editData(int ID, int STT, DateTime Ngay_GD, string Ma_CK, float Gia_DC, float TKL_GDKL_LoChan, float TGT_GDKL_LoChan, float TKL_GDKL_LoLe,
+            float TGT_GDKL_LoLe, float Tong_KLGD_TT_LoChan, float Tong_GTGD_TT_LoChan, float Tong_KLGD_TT_LoLe, float Tong_GTGD_TT_LoLe)
+        {
+            string query = "Update_GDTP @ID , @STT , @Ngay_GD , @Ma_CK , @Gia_DC , @TKL_GDKL_LoChan , @TGT_GDKL_LoChan , @TKL_GDKL_LoLe , @TGT_GDKL_LoLe , @Tong_KLGD_TT_LoChan , @Tong_GTGD_TT_LoChan , @Tong_KLGD_TT_LoLe , @Tong_GTGD_TT_LoLe";
+
+            int result = DataProvider.Instance.ExecuteNonquery(query, new object[] { ID , STT , Ngay_GD.ToString("MM/dd/yyyy"), Ma_CK, Gia_DC , TKL_GDKL_LoChan, TGT_GDKL_LoChan,
+            TKL_GDKL_LoLe, TGT_GDKL_LoLe, Tong_KLGD_TT_LoChan, Tong_GTGD_TT_LoChan,Tong_KLGD_TT_LoLe,Tong_GTGD_TT_LoLe});
+
+            return result > 0;
         }
 
         public void ImportExcelInDB(string filename)

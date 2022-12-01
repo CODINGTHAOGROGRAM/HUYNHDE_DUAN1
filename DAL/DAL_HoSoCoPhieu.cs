bd6ac158d866f5f22c95974c949bd2b7d9737c3c
LAMSAMCHAN <chan94170@gmail.com>
2022-12-01 14:44:24 +0700

    xong rồi

--- DAL/DAL_HoSoCoPhieu.cs
+++ DAL/DAL_HoSoCoPhieu.cs
@@ -25,8 +25,8 @@ namespace DAL
         //                                        @MaCk, @TenTCPH, @TruSoChinh, @DiaChiLienlac, @GPTL, @TenNganh, @NguoiDaiDien, @NguoiCongBo, @BanCaoBach, @TrangThaiKiemSoat, @TrangThaiGiaoDich, @NgayGDDauTien, @VonDieuLe, @KLLH, @KLNY, @Link_BanCaoBach
         public bool addDataHoSo(string MaCk, string TenTCPH, string TruSoChinh, string DiaChiLienlac, string GPTL, string TenNganh, string NguoiDaiDien, string NguoiCongBo, string BanCaoBach, string TrangThaiKiemSoat, string TrangThaiGiaoDich, DateTime NgayGDDauTien, float VonDieuLe, float KLLH, float KLNY, string Link_BanCaoBach)
         {
-            string queryProc = "Add_HSCP  @MaCk, @TenTCPH, @TruSoChinh, @DiaChiLienlac, @GPTL, @TenNganh, @NguoiDaiDien, @NguoiCongBo, @BanCaoBach, @TrangThaiKiemSoat, @TrangThaiGiaoDich, @NgayGDDauTien, @VonDieuLe, @KLLH, @KLNY, @Link_BanCaoBach ";
-            int rs = DataProvider.Instance.Executequery(queryProc, new object[] { MaCk, TenTCPH, TruSoChinh, DiaChiLienlac, GPTL, TenNganh, NguoiDaiDien, NguoiCongBo, BanCaoBach, TrangThaiKiemSoat, TrangThaiGiaoDich, NgayGDDauTien.ToString("MM/dd/yyyy"), VonDieuLe, KLLH, KLNY, Link_BanCaoBach });
+            string queryProc = "Add_HSCP  @MaCk , @TenTCPH , @TruSoChinh , @DiaChiLienlac , @GPTL , @TenNganh , @NguoiDaiDien , @NguoiCongBo , @BanCaoBach , @TrangThaiKiemSoat , @TrangThaiGiaoDich , @NgayGDDauTien , @VonDieuLe , @KLLH , @KLNY , @Link_BanCaoBach ";
+            int rs = DataProvider.Instance.ExecuteNonquery(queryProc, new object[] { MaCk, TenTCPH, TruSoChinh, DiaChiLienlac, GPTL, TenNganh, NguoiDaiDien, NguoiCongBo, BanCaoBach, TrangThaiKiemSoat, TrangThaiGiaoDich, NgayGDDauTien.ToString("MM/dd/yyyy"), VonDieuLe, KLLH, KLNY, Link_BanCaoBach });
             return rs > 0;
         }
     }
